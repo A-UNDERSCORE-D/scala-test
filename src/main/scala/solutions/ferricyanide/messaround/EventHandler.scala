8213a535074d9d04d428b697c8e9c96438d62e9a
A_D <A_D@snoonet.org>
2018-11-14 19:05:45 +0200

    added throwing of entities when a player is hit

--- src/main/scala/solutions/ferricyanide/messaround/EventHandler.scala
+++ src/main/scala/solutions/ferricyanide/messaround/EventHandler.scala
@@ -1,12 +1,16 @@
 package solutions.ferricyanide.messaround
 
 import net.minecraft.block.Block
+import net.minecraft.entity.EntityLivingBase
+import net.minecraft.entity.player.EntityPlayerMP
 import net.minecraft.item.{Item, ItemBlock}
+import net.minecraft.util.math.AxisAlignedBB
 import net.minecraftforge.event.RegistryEvent
+import net.minecraftforge.event.entity.living.LivingHurtEvent
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
-
+import scala.collection.JavaConverters.asScalaBufferConverter
 
 @Mod.EventBusSubscriber
 object EventHandler {
@@ -26,6 +30,25 @@ object EventHandler {
       val itemBlock = new ItemBlock(block)
       itemBlock.setRegistryName(block.getRegistryName)
       registry.register(itemBlock)
+    }
+  }
+
+  @SubscribeEvent
+  def onLivingHurt(event: LivingHurtEvent): Unit = {
+    val attacked = event.getEntity
+    val world = attacked.getEntityWorld
+
+    if (!attacked.isInstanceOf[EntityPlayerMP]) {
+      return
+    }
+
+    val area = new AxisAlignedBB(attacked.getPosition).expandXyz(64)
+    val mobsInArea = world.getEntitiesWithinAABB(classOf[EntityLivingBase], area).asScala
+    for (mob <- mobsInArea) {
+      if (!mob.isInstanceOf[EntityPlayerMP]) {
+        Messaround.logger.info(s"Throwing mob: $mob")
+        mob.addVelocity(0, 20D, 0)
+      }
     }
   }
 }
